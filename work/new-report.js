35691ec601ec0716394247ee082d8ac20fb9c9fb
Dirk Plate <email@dplate.de>
2022-09-03 17:43:50 +0200

    Add Heinzenberg report

--- work/new-report.js
+++ work/new-report.js
@@ -6,9 +6,9 @@ const sizeOf = require('image-size');
 const { DateTime } = require('luxon');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const sourcePath = 'D:/Bilder/2022/20220809_fürstensteig_1';
-const destination = 'malbun';
-const reportDate = '20220809';
+const sourcePath = 'D:/Bilder/2022/20220828_heinzenberg_1';
+const destination = 'lenzerheide';
+const reportDate = '20220828';
 
 const newPhoto = async (sourcePhoto, targetPhotosPath, index) => {
   const photoNumber = (index + 1).toLocaleString('en-US', {
