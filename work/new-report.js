666add696454b1da5651d897842e8f56ae2b5f09
Dirk Plate <email@dplate.de>
2024-09-28 15:49:55 +0200

    Add Margelchopf report

--- work/new-report.js
+++ work/new-report.js
@@ -6,9 +6,9 @@ const sizeOf = require('image-size');
 const { DateTime } = require('luxon');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const sourcePath = 'D:/Bilder/2024/20240907_igl-compass';
-const destination = 'davos';
-const reportDate = '20240907';
+const sourcePath = 'D:/Bilder/2024/20240921_margelkopf';
+const destination = 'alviergruppe';
+const reportDate = '20240921';
 
 const newPhoto = async (sourcePhoto, targetPhotosPath, index) => {
   const photoNumber = (index + 1).toLocaleString('en-US', {
