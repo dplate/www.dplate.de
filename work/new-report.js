eb6c179997aa62a45aa141c61dca834b58eb83a7
Dirk Plate <email@dplate.de>
2025-06-14 18:44:50 +0200

    Add Spitzmeilenhütte report

--- work/new-report.js
+++ work/new-report.js
@@ -6,9 +6,9 @@ const sizeOf = require('image-size');
 const { DateTime } = require('luxon');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const sourcePath = 'D:/Bilder/2025/20250510_rigi-2';
-const destination = 'vierwaldstaettersee';
-const reportDate = '20250510';
+const sourcePath = 'D:/Bilder/2025/20250609_spitzmeilenhütte';
+const destination = 'flumserberg';
+const reportDate = '20250609';
 
 const newPhoto = async (sourcePhoto, targetPhotosPath, index) => {
   const photoNumber = (index + 1).toLocaleString('en-US', {
