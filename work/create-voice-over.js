78ef1a997cd0d3ade8dcc3fba57392f79870b122
Dirk Plate <email@dplate.de>
2025-08-17 15:18:10 +0200

    Update Naafkopf article

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'alpstein';
-const reportDate = '20150912';
+const destination = 'malbun';
+const reportDate = '20161016';
 
 const client = new textToSpeech.TextToSpeechClient();
 
