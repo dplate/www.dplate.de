282febc8367ef4b9438d00ef0372fcd47041ffb1
Dirk Plate <email@dplate.de>
2025-11-23 16:58:59 +0100

    Update Kronberg and Bödele report

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'montafon';
-const reportDate = '20251114';
+const destination = 'boedele';
+const reportDate = '20140927';
 
 const client = new textToSpeech.TextToSpeechClient();
 
