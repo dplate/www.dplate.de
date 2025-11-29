a5fb3e5da1aa8acae5c84af1af14497e1fda7b0a
Dirk Plate <email@dplate.de>
2025-11-29 17:43:16 +0100

    Update Zwinglipass report

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'boedele';
-const reportDate = '20140927';
+const destination = 'alpstein';
+const reportDate = '20170617';
 
 const client = new textToSpeech.TextToSpeechClient();
 
