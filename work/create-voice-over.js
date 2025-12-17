56b458ac2476fd47c9dd4279fc1c94afd78022bd
Dirk Plate <email@dplate.de>
2025-12-17 14:02:18 +0100

    Update Gulmen report

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'davos';
-const reportDate = '20180610';
+const destination = 'toggenburg';
+const reportDate = '20180616';
 
 const client = new textToSpeech.TextToSpeechClient();
 
