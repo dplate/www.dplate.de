a98923b585a65925409fa8fc637f2d16111ee420
Dirk Plate <email@dplate.de>
2026-03-08 16:40:26 +0100

    Update Kopssee report

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'lenzerheide';
-const reportDate = '20191012';
+const destination = 'montafon';
+const reportDate = '20191013';
 
 const client = new textToSpeech.TextToSpeechClient();
 
@@ -158,7 +158,7 @@ const pronunciations = [
     ipa: 'ˈʃpiːçɐ'
   },
   {
-    regex: /(hinweg)/gi,
+    regex: /(Hinweg)/g,
     ipa: 'ˈhɪnvek'
   },
   {
