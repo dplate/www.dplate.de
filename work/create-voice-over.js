0176bd3bbce8ca4cb318c55d1e8a8462b00baa07
Dirk Plate <email@dplate.de>
2026-03-17 13:13:26 +0100

    Update Bödele reports

--- work/create-voice-over.js
+++ work/create-voice-over.js
@@ -3,8 +3,8 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 const projectPath = 'C:/Users/Roger/web/Dp3';
-const destination = 'praettigau';
-const reportDate = '20200919';
+const destination = 'boedele';
+const reportDate = '20201018';
 
 const client = new textToSpeech.TextToSpeechClient();
 
